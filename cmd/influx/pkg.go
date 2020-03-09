116feee2027c5ba7de6788a31c9d920a8c98a3b4
tigerater <tigerater@gmail.com>
2020-03-09 01:34:57 +0000

    515

--- cmd/influx/pkg.go
+++ cmd/influx/pkg.go
@@ -42,8 +42,7 @@ type cmdPkgBuilder struct {
 	hasColor        bool
 	hasTableBorders bool
 	meta            pkger.Metadata
-	orgID           string
-	org             string
+	org             organization
 	quiet           bool
 
 	applyOpts struct {
@@ -53,6 +52,7 @@ type cmdPkgBuilder struct {
 		resourceType string
 		buckets      string
 		dashboards   string
+		endpoints    string
 		labels       string
 		telegrafs    string
 		variables    string
@@ -94,8 +94,7 @@ func (b *cmdPkgBuilder) cmdPkgApply() *cobra.Command {
 	cmd.Flags().BoolVarP(&b.quiet, "quiet", "q", false, "disable output printing")
 	cmd.Flags().StringVar(&b.applyOpts.force, "force", "", `TTY input, if package will have destructive changes, proceed if set "true".`)
 
-	cmd.Flags().StringVarP(&b.orgID, "org-id", "", "", "The ID of the organization that owns the bucket")
-	cmd.Flags().StringVarP(&b.org, "org", "o", "", "The name of the organization that owns the bucket")
+	b.org.register(cmd)
 
 	cmd.Flags().BoolVarP(&b.hasColor, "color", "c", true, "Enable color in output, defaults true")
 	cmd.Flags().BoolVar(&b.hasTableBorders, "table-borders", true, "Enable table borders, defaults true")
@@ -105,18 +104,9 @@ func (b *cmdPkgBuilder) cmdPkgApply() *cobra.Command {
 	return cmd
 }
 
-func (b *cmdPkgBuilder) validOrgFlags() error {
-	if b.orgID == "" && b.org == "" {
-		return fmt.Errorf("must specify org-id, or org name")
-	} else if b.orgID != "" && b.org != "" {
-		return fmt.Errorf("must specify org-id, or org name not both")
-	}
-	return nil
-}
-
 func (b *cmdPkgBuilder) pkgApplyRunEFn() func(*cobra.Command, []string) error {
 	return func(cmd *cobra.Command, args []string) (e error) {
-		if err := b.validOrgFlags(); err != nil {
+		if err := b.org.validOrgFlags(); err != nil {
 			return err
 		}
 		color.NoColor = !b.hasColor
@@ -126,7 +116,11 @@ func (b *cmdPkgBuilder) pkgApplyRunEFn() func(*cobra.Command, []string) error {
 			return err
 		}
 
-		influxOrgID, err := getOrgID(orgSVC, b.orgID, b.org)
+		if err := b.org.validOrgFlags(); err != nil {
+			return err
+		}
+
+		influxOrgID, err := b.org.getID(orgSVC)
 		if err != nil {
 			return nil
 		}
@@ -231,6 +225,7 @@ func (b *cmdPkgBuilder) cmdPkgExport() *cobra.Command {
 	cmd.Flags().StringVar(&b.exportOpts.resourceType, "resource-type", "", "The resource type provided will be associated with all IDs via stdin.")
 	cmd.Flags().StringVar(&b.exportOpts.buckets, "buckets", "", "List of bucket ids comma separated")
 	cmd.Flags().StringVar(&b.exportOpts.dashboards, "dashboards", "", "List of dashboard ids comma separated")
+	cmd.Flags().StringVar(&b.exportOpts.endpoints, "endpoints", "", "List of notification endpoint ids comma separated")
 	cmd.Flags().StringVar(&b.exportOpts.labels, "labels", "", "List of label ids comma separated")
 	cmd.Flags().StringVar(&b.exportOpts.telegrafs, "telegraf-configs", "", "List of telegraf config ids comma separated")
 	cmd.Flags().StringVar(&b.exportOpts.variables, "variables", "", "List of variable ids comma separated")
@@ -255,6 +250,7 @@ func (b *cmdPkgBuilder) pkgExportRunEFn() func(*cobra.Command, []string) error {
 		}{
 			{kind: pkger.KindBucket, idStrs: strings.Split(b.exportOpts.buckets, ",")},
 			{kind: pkger.KindDashboard, idStrs: strings.Split(b.exportOpts.dashboards, ",")},
+			{kind: pkger.KindNotificationEndpoint, idStrs: strings.Split(b.exportOpts.endpoints, ",")},
 			{kind: pkger.KindLabel, idStrs: strings.Split(b.exportOpts.labels, ",")},
 			{kind: pkger.KindTelegraf, idStrs: strings.Split(b.exportOpts.telegrafs, ",")},
 			{kind: pkger.KindVariable, idStrs: strings.Split(b.exportOpts.variables, ",")},
@@ -297,8 +293,9 @@ func (b *cmdPkgBuilder) cmdPkgExportAll() *cobra.Command {
 	cmd.Short = "Export all existing resources for an organization as a package"
 
 	cmd.Flags().StringVarP(&b.file, "file", "f", "", "output file for created pkg; defaults to std out if no file provided; the extension of provided file (.yml/.json) will dictate encoding")
-	cmd.Flags().StringVarP(&b.orgID, "org-id", "", "", "organization id")
-	cmd.Flags().StringVarP(&b.org, "org", "o", "", "The name of the organization that owns the bucket")
+
+	b.org.register(cmd)
+
 	cmd.Flags().StringVarP(&b.meta.Name, "name", "n", "", "name for new pkg")
 	cmd.Flags().StringVarP(&b.meta.Description, "description", "d", "", "description for new pkg")
 	cmd.Flags().StringVarP(&b.meta.Version, "version", "v", "", "version for new pkg")
@@ -317,7 +314,7 @@ func (b *cmdPkgBuilder) pkgExportAllRunEFn() func(*cobra.Command, []string) erro
 
 		opts := []pkger.CreatePkgSetFn{pkger.CreateWithMetadata(b.meta)}
 
-		orgID, err := getOrgID(orgSVC, b.orgID, b.org)
+		orgID, err := b.org.getID(orgSVC)
 		if err != nil {
 			return err
 		}
