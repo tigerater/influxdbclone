0503ba88e5fd9b1bdb87a3ee673c4619ba1705c9
tigerater <tigerater@gmail.com>
2020-03-09 01:22:38 +0000

    628

--- query/logging.go
+++ query/logging.go
@@ -11,7 +11,6 @@ import (
 	"github.com/influxdata/flux/iocounter"
 	"github.com/influxdata/influxdb/kit/check"
 	"github.com/influxdata/influxdb/kit/tracing"
-	"github.com/influxdata/influxdb/logger"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
@@ -51,11 +50,8 @@ func (s *LoggingProxyQueryService) Query(ctx context.Context, w io.Writer, req *
 				entry.Write(zap.Error(err))
 			}
 		}
-		traceID, sampled, _ := logger.TraceInfo(ctx)
 		log := Log{
 			OrganizationID: req.Request.OrganizationID,
-			TraceID:        traceID,
-			Sampled:        sampled,
 			ProxyRequest:   req,
 			ResponseSize:   n,
 			Time:           s.nowFunction(),
